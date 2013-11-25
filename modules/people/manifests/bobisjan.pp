70bf360f7ac1312d2fa8f53faa2550d5cf564e93
Jan Bobisud <me@bobisjan.com>
2013-11-25 22:48:51 +0100

    Comment not working includes

--- modules/people/manifests/bobisjan.pp
+++ modules/people/manifests/bobisjan.pp
@@ -3,10 +3,11 @@ class people::bobisjan {
     include osx::global::disable_remote_control_ir_receiver
     include osx::finder::show_all_on_desktop
     include osx::finder::unhide_library
-    
-    include github::apps::mac
+
+#    include github::apps::mac
+
     include dropbox
-    include skype
+#    include skype
 
     $home = "/Users/${::boxen_user}"
 
