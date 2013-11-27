0deafd533676dc29f53f3492b2b057e08d15e7dd
Jan Bobisud <me@bobisjan.com>
2013-11-27 19:22:18 +0100

    Use tilde for user's home folder reference
    Error: Cannot create /Users/#{user}/Developer; parent directory /Users/#{user} does not exist
    Error: /Stage[main]/Boxen::Config/File[/Users/#{user}/Developer]/ensure: change from absent to directory failed: Cannot create /Users/#{user}/Developer; parent directory /Users/#{user} does not exist

--- config/boxen.rb
+++ config/boxen.rb
@@ -8,7 +8,7 @@ ENV['BOXEN_HOME'] = '/opt/boxen'
 ENV['BOXEN_REPO_NAME'] = 'bobisjan/boxen'
 
 # Change the project repos directory.
-ENV['BOXEN_SRC_DIR'] = '/Users/#{user}/Developer'
+ENV['BOXEN_SRC_DIR'] = '~/Developer'
 
 # Boxen binary packaging
 # ENV["BOXEN_S3_ACCESS_KEY"] = ''
