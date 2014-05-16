f63397f780e3b823dc89d3eb754b8ea9c4dbbf89
Jan Bobisud <me@bobisjan.com>
2014-05-16 09:05:50 +0200

    Add more apps
    Chrome, Induction, IntelliJ, Postgres, Ember-CLI and Bower

--- modules/people/manifests/bobisjan.pp
+++ modules/people/manifests/bobisjan.pp
@@ -4,24 +4,27 @@ class people::bobisjan {
     include osx::finder::show_all_on_desktop
     include osx::finder::unhide_library
 
-    include git
-
-    include heroku
-
-    include github_for_mac
-
+    include chrome
     include cocoapods
-    include testflight
     include dropbox
+    include git
+    include github_for_mac
+    include heroku
+    include induction
+    include postgresapp
     include skype
+    include testflight
+
+    class { 'intellij':
+      edition => 'ultimate',
+      version => '13.1.2'
+    }
+
+    git::config::global { 'user.email': value  => 'me@bobisjan.com' }
+    git::config::global { 'user.name': value  => 'Jan Bobisud' }
+
+    nodejs::module { 'ember-cli': node_version => 'v0.10' }
+    nodejs::module { 'bower': node_version => 'v0.10' }
 
     $home = "/Users/${::boxen_user}"
-    
-    git::config::global {'user.email':
-      value  => 'me@bobisjan.com'
-    }
-    
-    git::config::global {'user.name':
-      value  => 'Jan Bobisud'
-    }
 }
