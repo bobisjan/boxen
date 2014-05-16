4c47a1adbc736a68b07ab88e3a71ae248db14b98
Jan Bobisud <me@bobisjan.com>
2014-05-16 08:32:32 +0200

    Remove Ruby 1.9.3

--- manifests/site.pp
+++ manifests/site.pp
@@ -67,7 +67,6 @@ node default {
   include nodejs::v0_10
 
   # default ruby versions
-  ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
