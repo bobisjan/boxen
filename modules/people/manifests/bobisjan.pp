90bf49eb7337abd09d77db2545df387163dbb291
Jan Bobisud <me@bobisjan.com>
2014-10-19 00:58:33 +0200

    Bring back people/projects manifests

---
+++ modules/people/manifests/bobisjan.pp
@@ -0,0 +1,33 @@
+
+class people::bobisjan {
+    include osx::global::disable_remote_control_ir_receiver
+    include osx::finder::show_all_on_desktop
+    include osx::finder::unhide_library
+
+    include atom
+    include chrome
+    include cocoapods
+    include dropbox
+    include firefox
+    include git
+    include github_for_mac
+    include heroku
+    include induction
+    include java
+    include phantomjs
+    include postgresapp
+    include skype
+    include testflight
+
+    git::config::global { 'user.email': value  => 'me@bobisjan.com' }
+    git::config::global { 'user.name': value  => 'Jan Bobisud' }
+
+    class { 'intellij': edition => 'ultimate', version => '13.1.2' }
+
+    nodejs::module { 'ember-cli': node_version => 'v0.10' }
+    nodejs::module { 'bower': node_version => 'v0.10' }
+
+    phantomjs::global { '1.9.0': }
+
+    $home = "/Users/${::boxen_user}"
+}
