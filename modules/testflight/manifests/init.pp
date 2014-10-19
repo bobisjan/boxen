831c2a5d98ec22133ac68fa4356af405a5d72339
Jan Bobisud <me@bobisjan.com>
2014-10-19 08:51:28 +0200

    Remove items from modules

--- modules/testflight/manifests/init.pp
+++
@@ -1,6 +0,0 @@
-class testflight {
-  package { 'testflight':
-    source   => 'https://d193ln56du8muy.cloudfront.net/desktop_app/1348796279/TestFlight-Desktop-1.0-Beta(296).zip',
-    provider => 'compressed_app'
-  }
-}
