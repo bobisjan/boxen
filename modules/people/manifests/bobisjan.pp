21b3807c5e9d595f79d6c76ced139ccea3f17f1d
Jan Bobisud <me@bobisjan.com>
2014-12-20 13:48:50 +0100

    Install compass, watchman, grunt and grunt-cli

--- modules/people/manifests/bobisjan.pp
+++ modules/people/manifests/bobisjan.pp
@@ -23,12 +23,27 @@ class people::bobisjan {
 
     package { 'bash-completion': ensure => present }
 
+    # set the global ruby version
+    $ruby_version = '2.1.2'
+    class { 'ruby::global': version => $ruby_version }
+
+    ruby_gem { "compass for ${ruby_version}":
+      gem => 'compass',
+      version => '~> 1.0',
+      ruby_version => $ruby_version,
+    }
+
     # set the global nodejs version
-    class { 'nodejs::global': version => 'v0.10' }
+    $node_version = 'v0.10'
+    class { 'nodejs::global': version => $node_version }
 
-    nodejs::module { 'ember-cli': node_version => 'v0.10' }
-    nodejs::module { 'bower': node_version => 'v0.10' }
+    nodejs::module { 'bower': node_version => $node_version }
+    nodejs::module { 'watchman': node_version => $node_version }
+    nodejs::module { 'ember-cli': node_version => $node_version }
+    nodejs::module { 'grunt': node_version => $node_version }
+    nodejs::module { 'grunt-cli': node_version => $node_version }
 
+    # set the global phantomjs version
     phantomjs::global { '1.9.0': }
 
     $home = "/Users/${::boxen_user}"
